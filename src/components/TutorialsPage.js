73362bf762d72208542b0c008f2acf47786a272c
rxv3774 <rxv3774@g.rit.edu>
2020-03-18 16:59:24 -0400

    Added video player functionality

--- src/components/TutorialsPage.js
+++ src/components/TutorialsPage.js
@@ -5,8 +5,8 @@ import MoolahNavBar from './MoolahNavBar.js';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../node_modules/video-react/dist/video-react.css";
+import ReactPlayer from "react-player";
 
-import { Player } from 'video-react';
 
 class TutorialsPage extends React.Component{
   render(){
@@ -15,73 +15,75 @@ class TutorialsPage extends React.Component{
         <MoolahNavBar />
         <br></br>
         <h2 style={{paddingLeft: '600px', paddingBottom: '100px'}}>Financial Tutorials</h2>
-        <div style={{backgroundColor:'#DFF3F7', paddingRight:'500px', paddingLeft: '200px', display: 'grid', gridTemplateColumns: '500px 500px',
+        <div style={{backgroundColor:'#DFF3F7', paddingRight:'200px', paddingLeft: '200px', display: 'grid', gridTemplateColumns: '500px 500px',
         gridGap: '20px', borderRadius: '5px', columnCount: 2}}>
          
-         <div><Player
-            playsIncline
-            poster=""
-            src=""
+         <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=5DXKOlr7SeI"
             />
             <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Budgeting Expenses</h6>
             </div>
 
-          <div><Player
-            playsIncline
-            poster=""
-            src=""
+          <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=flG4gFIiZzU"
             />
             <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Student Loans</h6>
             </div>
 
-            <div><Player
-            playsIncline
-            poster=""
-            src=""
+            <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=BeAou8wXagw"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            <h6 style={{paddingLeft: '100px', paddingTop: '10px', paddingBottom: '10px'}}>Spending Less Throughout the Week</h6>
             </div>
 
-            <div><Player
-            playsIncline
-            poster=""
-            src=""
+            <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=HQzoZfc3GwQ"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            <h6 style={{paddingLeft: '170px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Savings</h6>
             </div>
 
-            <div><Player
-            playsIncline
-            poster=""
-            src=""
+            <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=Y77B8kaRS1g"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            <h6 style={{paddingLeft: '170px', paddingTop: '10px', paddingBottom: '10px'}}>Creating a Budget</h6>
             </div>
 
-            <div><Player
-            playsIncline
-            poster=""
-            src=""
+            <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=rqPIG3QcrDE"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            <h6 style={{paddingLeft: '160px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Your Budget</h6>
             </div>
 
-            <div><Player
-            playsIncline
-            poster=""
-            src=""
+            <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=Nqs0CvY-PmI"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            <h6 style={{paddingLeft: '130px', paddingTop: '10px', paddingBottom: '10px'}}>Student Loan Payment Options</h6>
             </div>
 
-            <div><Player
-            playsIncline
-            poster=""
-            src=""
+            <div><ReactPlayer
+            height="300px"
+            width="500px"
+            url="https://www.youtube.com/watch?v=e-LpPTnme_M"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            <h6 style={{paddingLeft: '160px', paddingTop: '10px', paddingBottom: '10px'}}>Student Loan Alleviation</h6>
             </div>
+
         </div>
+
       </div>
     );
   }
