57110f804fd469f735b8cf1667de5788466274d4
rxv3774 <rxv3774@g.rit.edu>
2020-03-17 20:17:26 -0400

    Added inital video player

--- src/components/TutorialsPage.js
+++ src/components/TutorialsPage.js
@@ -4,13 +4,84 @@ import '../stylesheets/home.css';
 import MoolahNavBar from './MoolahNavBar.js';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../node_modules/video-react/dist/video-react.css";
+
+import { Player } from 'video-react';
 
 class TutorialsPage extends React.Component{
   render(){
     return (
       <div style={{backgroundColor:'#DFF3F7'}}>
         <MoolahNavBar />
-        Tutorials!
+        <br></br>
+        <h2 style={{paddingLeft: '600px', paddingBottom: '100px'}}>Financial Tutorials</h2>
+        <div style={{backgroundColor:'#DFF3F7', paddingRight:'500px', paddingLeft: '200px', display: 'grid', gridTemplateColumns: '500px 500px',
+        gridGap: '20px', borderRadius: '5px', columnCount: 2}}>
+         
+         <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Budgeting Expenses</h6>
+            </div>
+
+          <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Student Loans</h6>
+            </div>
+
+            <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            </div>
+
+            <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            </div>
+
+            <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            </div>
+
+            <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            </div>
+
+            <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            </div>
+
+            <div><Player
+            playsIncline
+            poster=""
+            src=""
+            />
+            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Tutorial</h6>
+            </div>
+        </div>
       </div>
     );
   }
