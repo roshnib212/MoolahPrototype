d6bec3ce2a83a14a2e3ec7ebfa14b03bd378cafb
Ji Sook Moon <jisook.moon@Jis-MacBook-Pro.local>
2020-03-18 03:34:23 -0400

    basic graphs

--- src/components/OverviewPage.js
+++ src/components/OverviewPage.js
@@ -4,13 +4,155 @@ import '../stylesheets/home.css';
 import MoolahNavBar from './MoolahNavBar.js';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Pie } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import { MDBContainer } from "mdbreact";
 
 class OverviewPage extends React.Component{
+  pieState = {
+    dataPie: {
+      labels: ["Shopping", "Necessities", "Investments", "Transportation", "Dining"],
+      datasets: [
+        {
+          data: [300, 50, 100, 40, 120],
+          backgroundColor: [
+            "#F7464A",
+            "#46BFBD",
+            "#FDB45C",
+            "#949FB1",
+            "#4D5360",
+            "#AC64AD"
+          ],
+          hoverBackgroundColor: [
+            "#FF5A5E",
+            "#5AD3D1",
+            "#FFC870",
+            "#A8B3C5",
+            "#616774",
+            "#DA92DB"
+          ]
+        }
+      ]
+    }
+  }
+barState = {
+    dataBar: {
+      labels: ["Last Month Earnings", "Last Month Spendings", "Current Month Earnings", "Current Month Spendings"],
+      datasets: [
+        {
+          label: "Dollars",
+          data: [100, 80, 100, 40],
+          backgroundColor: [
+            "rgba(255, 134,159,0.4)",
+            "rgba(98,  182, 239,0.4)",
+            "rgba(255, 218, 128,0.4)",
+            "rgba(113, 205, 205,0.4)",
+          ],
+          borderWidth: 2,
+          borderColor: [
+            "rgba(255, 134, 159, 1)",
+            "rgba(98,  182, 239, 1)",
+            "rgba(255, 218, 128, 1)",
+            "rgba(113, 205, 205, 1)",
+          ]
+        }
+      ]
+    },
+    barChartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            barPercentage: 1,
+            gridLines: {
+              display: true,
+              color: "rgba(0, 0, 0, 0.1)"
+            }
+          }
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: true,
+              color: "rgba(0, 0, 0, 0.1)"
+            },
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  }
+lineState = {
+    dataLine: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Earnings",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(225, 204,230, .3)",
+          borderColor: "rgb(205, 130, 158)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(205, 130,1 58)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [100, 100, 150, 150, 200, 200, 220]
+        },
+        {
+          label: "Savings",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(184, 185, 210, .3)",
+          borderColor: "rgb(35, 26, 136)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(35, 26, 136)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [20, 20, 30, 30, 40, 40, 44]
+        }
+      ]
+    }
+  };
+
   render(){
     return (
-      <div style={{backgroundColor:'#DFF3F7'}}>
+      <div style={{backgroundColor:'#DFF3F7', height: '1800px'}}>
         <MoolahNavBar />
-        Overview!
+      <MDBContainer>
+        <h3 className="mt-5">Financial Summary</h3>
+	    Your Spending Breakdown
+        <Pie data={this.pieState.dataPie} options={{ responsive: true }} />
+      </MDBContainer>
+      <MDBContainer>
+        <h3 className="mt-5">Spending vs Earnings</h3>
+        <Bar data={this.barState.dataBar} options={this.barState.barChartOptions} />
+      </MDBContainer>
+<MDBContainer>
+        <h3 className="mt-5">Savings</h3>
+        <Line data={this.lineState.dataLine} options={{ responsive: true }} />
+      </MDBContainer>
       </div>
     );
   }
