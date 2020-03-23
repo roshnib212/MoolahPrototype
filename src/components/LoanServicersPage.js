46d8792b7c5ad35c85c9e09d7cbe40dce95d998f
Roshni Barodia <roshnibarodia@Roshnis-MacBook-Pro.local>
2020-03-23 19:05:35 -0400

    adding some random spaces

--- src/components/LoanServicersPage.js
+++ src/components/LoanServicersPage.js
@@ -84,12 +84,12 @@ class LoanServicersPage extends React.Component{
 
   render(){
     return (
-      <div style={{backgroundColor:'#DFF3F7', height: '100%'}} className="text-center">
+      <div style={{backgroundColor:'#DFF3F7', height: '100%', overflow: 'auto', paddingBottom: '75px'}} className="text-center">
         <MoolahNavBar />
         <br></br>
         <h3>Federal Student Aid Servicers</h3>
         <br></br>
-        <div style={{backgroundColor:'#DFF3F7'}} className="text-left">
+        <div style={{backgroundColor:'#DFF3F7', paddingLeft: '200px', paddingRight: '200px'}} className="text-left">
           {this.state.listServicers.map(servicer => (
             <Card>
               <Card.Body>
@@ -102,9 +102,11 @@ class LoanServicersPage extends React.Component{
             </Card>
           ))}
         </div>
+        <br></br>
         <div class="d-flex justify-content-center">
           <Row>
             <Button style = {{margin: 0, alignSelf: 'center'}} variant="danger" onClick={this.handleAddNewServicer}>Remove Loan Servicer</Button>
+            &nbsp;&nbsp;&nbsp;
             <Button style = {{margin: 0, alignSelf: 'center'}} variant="primary" onClick={this.handleAddNewServicer}>Add Loan Servicer</Button>
           </Row>
         </div>
@@ -117,7 +119,7 @@ class LoanServicersPage extends React.Component{
         <br></br>
         <h3>Private Student Loan Lenders</h3>
         <br></br>
-        <div style={{backgroundColor:'#DFF3F7'}} className="text-left">
+        <div style={{backgroundColor:'#DFF3F7', paddingLeft: '200px', paddingRight: '200px'}} className="text-left">
           {this.state.listLenders.map(lender => (
             <Card>
               <Card.Body>
@@ -130,9 +132,11 @@ class LoanServicersPage extends React.Component{
             </Card>
           ))}
         </div>
+        <br></br>
         <div class="d-flex justify-content-center">
           <Row>
             <Button style = {{margin: 0, alignSelf: 'center'}} variant="danger" onClick={this.handleAddNewLender}>Remove Loan Lender</Button>
+            &nbsp;&nbsp;&nbsp;
             <Button style = {{margin: 0, alignSelf: 'center'}} variant="primary" onClick={this.handleAddNewLender}>Add Loan Lender</Button>
           </Row>
         </div>
