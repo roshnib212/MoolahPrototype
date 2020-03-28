3aebd1aca0cfad05664d6c6a4359c49db51dbcd1
rxv3774 <rxv3774@g.rit.edu>
2020-03-27 23:00:38 -0400

    Added create a budge functionality

--- src/components/BudgetPage.js
+++ src/components/BudgetPage.js
@@ -9,6 +9,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Modal from 'react-bootstrap/Modal';
+import { MDBContainer, MDBInput } from "mdbreact";
 
 class BudgetPage extends React.Component{
   constructor(props){
@@ -17,9 +18,7 @@ class BudgetPage extends React.Component{
       diningBudget: 0,
       entertainmentBudget: 0,
       shoppingBudget: 0,
-      income: null,
-      residential_status: null,
-      household_size: null,
+      percentage: 0,
       modalShow: false
     }
     this.handleEdit = this.handleEdit.bind(this);
@@ -51,14 +50,14 @@ class BudgetPage extends React.Component{
         <h2 style={{paddingLeft: "550px", paddingTop: "40px"}}>Your Current Budgets</h2>
         <br></br>
 
-        <div>
+        <div style={{paddingLeft: "430px", paddingTop: "50px"}}>
           <Row>
             <Col>
               <h5>Dining: </h5>
             </Col>
             <Col>
               <p>
-                {this.state.diningBudget}
+                {"$" + this.state.diningBudget}
               </p>
             </Col>
           </Row>
@@ -68,7 +67,7 @@ class BudgetPage extends React.Component{
             </Col>
             <Col>
               <p>
-                {this.state.entertainmentBudget}
+                {"$" + this.state.entertainmentBudget}
               </p>
             </Col>
           </Row>
@@ -78,14 +77,18 @@ class BudgetPage extends React.Component{
             </Col>
             <Col>
               <p>
-                {this.state.shoppingBudget}
+                {"$" + this.state.shoppingBudget}
               </p>
             </Col>
           </Row>
           
           <br></br>
           <Button variant="primary" onClick={this.handleEdit}>Create a new Budget</Button>
-          
+          <EditModal
+            show={this.state.modalShow}
+            onHide={this.closeModal}
+            onSubmit={this.handleSubmitForm}
+          />
 
         </div>
 
@@ -94,6 +97,124 @@ class BudgetPage extends React.Component{
   }
 }
 
+class EditModal extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      diningBudget: 0,
+      entertainmentBudget: 0,
+      shoppingBudget: 0,
+      radio: 1
+    }
+    this.handleDiningChange = this.handleDiningChange.bind(this);
+    this.handleEntertainmentChange = this.handleEntertainmentChange.bind(this);
+    this.handleShoppingChange = this.handleShoppingChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.onSubmit(this.state.diningBudget, this.state.entertainmentBudget,
+       this.state.shoppingBudget);
+  }
+  handleDiningChange(event){
+    this.setState({
+      diningBudget: event.target.value
+    });
+  }
+  handleEntertainmentChange(event){
+    this.setState({
+      entertainmentBudget: event.target.value
+    });
+  }
+  handleShoppingChange(event){
+    this.setState({
+      shoppingBudget: event.target.value
+    });
+  }
+  onRadioCheck = nr => () => {
+    this.setState({
+      radio: nr
+    });
+  }
+  render(){
+    return(
+      <Modal show={this.props.show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Create a new Budget
+          </Modal.Title>
+          <button onClick={this.props.onHide} type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={this.handleSubmit}>
+            <Row>
+              <Col>
+                <Form.Label>Dining Budget</Form.Label>
+              </Col>
+
+              <Form.Label style={{paddingTop: "5px"}}>$</Form.Label>
+
+              <Col>
+                <Form.Group>
+                  <Form.Control value={this.state.diningBudget} onChange={this.handleDiningChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Label>Entertainment Budget</Form.Label>
+              </Col>
+              
+                <Form.Label style={{paddingTop: "5px"}}>$</Form.Label>
+              
+              <Col>
+                <Form.Group>
+                  <Form.Control value={this.state.entertainmentBudget} onChange={this.handleEntertainmentChange}/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                  <Form.Label>Shopping Budget</Form.Label>
+              </Col>
+
+              <Form.Label style={{paddingTop: "5px"}}>$</Form.Label>
+
+              <Col>
+                <Form.Control value={this.state.shoppingBudget} onChange={this.handleShoppingChange}/>
+
+                <br></br>
+                
+                <Row>
+                  How Often?:
+                </Row>
+
+                <br></br>
+                <div>
+                  <input onClick={this.onRadioCheck(1)} checked={this.state.radio===1 ? true : false} type="radio"/>
+                  <label style={{paddingLeft: "5px"}}>Weekly</label>
+                </div>
+                <div>
+                  <input onClick={this.onRadioCheck(2)} checked={this.state.radio===2 ? true : false} type="radio"/>
+                  <label style={{paddingLeft: "5px"}}>Monthly</label>
+                </div>
+              </Col>
+
+              
+            </Row>
+            &nbsp;&nbsp;&nbsp;
+            
+            <Button variant="success" type="submit">Submit</Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+    </Modal>
+  );
+  }
+}
 
 export default BudgetPage;
