929e029b90c5d6ceb9c4e96ec4a1ff255c584ec4
rxv3774 <rxv3774@g.rit.edu>
2020-04-06 23:01:54 -0400

    first rough draft for all videos

--- src/components/TutorialsPage.js
+++ src/components/TutorialsPage.js
@@ -51,61 +51,89 @@ class TutorialsPage extends React.Component{
             <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
           </ul>
 
-          <ul><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=flG4gFIiZzU"
             />
-            <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Student Loans</h6>
+            <h5 style={{paddingLeft: '10px'}}>Managing Student Loans</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 2 Weeks Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
           </ul>
 
-            <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=BeAou8wXagw"
             />
-            <h6 style={{paddingLeft: '100px', paddingTop: '10px', paddingBottom: '10px'}}>Spending Less Throughout the Week</h6>
-            </div>
+            <h5 style={{paddingLeft: '10px'}}>Spending Less Throughout the Week</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
-            <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=HQzoZfc3GwQ"
             />
-            <h6 style={{paddingLeft: '170px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Savings</h6>
-            </div>
+            <h5 href style={{paddingLeft: '10px'}}>Managing Savings</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
-            <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=Y77B8kaRS1g"
             />
-            <h6 style={{paddingLeft: '170px', paddingTop: '10px', paddingBottom: '10px'}}>Creating a Budget</h6>
-            </div>
+            <h5 style={{paddingLeft: '10px'}}>Creating a Budget</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
-            <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=rqPIG3QcrDE"
             />
-            <h6 style={{paddingLeft: '160px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Your Budget</h6>
-            </div>
+            <h5 style={{paddingLeft: '10px'}}>Managing Your Budget</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
-            <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=Nqs0CvY-PmI"
             />
-            <h6 style={{paddingLeft: '130px', paddingTop: '10px', paddingBottom: '10px'}}>Student Loan Payment Options</h6>
-            </div>
+            <h5 style={{paddingLeft: '10px'}}>Student Loan Payment Options</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
-            <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=Iat9AuCl_nU"
             />
-            <h6 style={{paddingLeft: '160px', paddingTop: '10px', paddingBottom: '10px'}}>Student Loan Alleviation</h6>
-            </div>
+            <h5 style={{paddingLeft: '10px'}}>Student Loan Alleviation</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
         </div>
       </div>
