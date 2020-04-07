b85b5fa6b851ac06d16003147f76bb6585735d23
rxv3774 <rxv3774@g.rit.edu>
2020-04-06 22:35:06 -0400

    styled rough draft for first video

--- src/components/TutorialsPage.js
+++ src/components/TutorialsPage.js
@@ -7,30 +7,57 @@ import ReactPlayer from "react-player";
 
 
 class TutorialsPage extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    }
+  }
+
+  updateSearch(event) {
+
+  }
+
   render(){
+
+    //let videos = [{
+    //  
+    //}]
+//
+    //let filteredResults = this.props.videos.filter(
+    //  (video) => {
+    //    return video.name.indexOf(this.state.search) !== -1;
+    //  }
+    //);
     return (
       <div style={{backgroundColor:'#DFF3F7'}}>
         <MoolahNavBar />
         <br></br>
         <h2 style={{paddingLeft: '600px', paddingBottom: '100px', backgroundColor: ""}}>Financial Tutorials</h2>
-        <div style={{backgroundColor:'#DFF3F7', paddingRight:'200px', paddingLeft: '200px', display: 'grid', gridTemplateColumns: '500px 500px',
-        gridGap: '20px', borderRadius: '5px', columnCount: 2}}>
+        <div style={{backgroundColor:'#DFF3F7', paddingRight:'200px', paddingLeft: '200px',
+        borderRadius: '5px', columnCount: 1, alignItems: 'center'}}>
 
-         <div><ReactPlayer
-            height="300px"
-            width="500px"
+          <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+
+          <ul style={{display: 'grid', gridTemplateColumns: '250px 500px', gridTemplateRows: '60px 20px 90px'}}>
+            <ReactPlayer
+            style={{gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 1, gridRowEnd: 4}}
+            height="100%"
+            width="100%"
             url="https://www.youtube.com/watch?v=5DXKOlr7SeI"
             />
-            <h6 style={{paddingLeft: '160px', paddingTop: '10px', paddingBottom: '10px'}}>Budgeting Expenses</h6>
-            </div>
+            <h5 style={{paddingLeft: '10px'}}>How To Budget And Save Money</h5>
+            <p style={{paddingLeft: '10px'}}>Posted: 1 Week Ago</p>
+            <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>This is an example description! This video represents how to budget your blah blah blah in a more organized manner!</p>
+          </ul>
 
-          <div><ReactPlayer
+          <ul><ReactPlayer
             height="300px"
             width="500px"
             url="https://www.youtube.com/watch?v=flG4gFIiZzU"
             />
             <h6 style={{paddingLeft: '150px', paddingTop: '10px', paddingBottom: '10px'}}>Managing Student Loans</h6>
-            </div>
+          </ul>
 
             <div><ReactPlayer
             height="300px"
