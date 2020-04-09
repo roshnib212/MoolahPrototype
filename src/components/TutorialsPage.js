b7bd99ab0842853976df04be184d22b9cca26eda
rxv3774 <rxv3774@g.rit.edu>
2020-04-08 22:53:00 -0400

    fixed some styling and added video descriptions :)

--- src/components/TutorialsPage.js
+++ src/components/TutorialsPage.js
@@ -16,49 +16,49 @@ class TutorialsPage extends React.Component{
           url: "https://www.youtube.com/watch?v=5DXKOlr7SeI",
           title: 'How To Budget And Save Money',
           posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          description: 'In this video, Stefan provides you with some money management tips for how to budget and save money. Saving is very important. You have to make sure that you are putting money aside every month.'
         },
         {
           url: "https://www.youtube.com/watch?v=flG4gFIiZzU",
           title: 'Managing Student Loans',
           posted: 2,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          description: 'Check out this video to learn more about changing repayment plans, postponing or reducing your payments, or combining your federal student loans.'
         },
         {
           url: "https://www.youtube.com/watch?v=BeAou8wXagw",
           title: 'Spending Less Throughout the Week',
           posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          description: 'Do you have a problem with impulse spending and shopping? Do you buy things that you dont need? In this video, we will share 15 easy and practical tips to stop spending money aimlessly.'
         },
         {
           url: "https://www.youtube.com/watch?v=HQzoZfc3GwQ",
           title: 'Managing Savings',
           posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          description: 'In this video we present a high level overview on how to manage your money using the 50/30/20 Rule. Money management is 90% discipline and 10% knowledge. The 50/30/20 rule will help you to create and manage a budget.'
         },
         {
           url: "https://www.youtube.com/watch?v=Y77B8kaRS1g",
           title: 'Creating a Budget',
           posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          description: 'These basic tips will teach you how to make a budget and save money.'
         },
         {
           url: "https://www.youtube.com/watch?v=rqPIG3QcrDE",
           title: 'Managing Your Budget',
-          posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          posted: 2,
+          description: 'Learn the basics of planning between paychecks in order to help you save up some money!'
         },
         {
           url: "https://www.youtube.com/watch?v=Nqs0CvY-PmI",
           title: 'Student Loan Payment Options',
           posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          description: 'Understanding how to repay your federal student loans can save you a lot of time and money. There are several repayment plans available, providing the flexibility you need.'
         },
         {
           url: "https://www.youtube.com/watch?v=Iat9AuCl_nU",
           title: 'Student Loan Alleviation',
-          posted: 1,
-          description: 'This is an example description! This video represents how to budget your blah blah blah in a more organized manner!'
+          posted: 3,
+          description: 'In this video we will discuss the basics of student loans so you can graduate in the best financial position possible.'
         },
       ]
     }
@@ -81,7 +81,7 @@ class TutorialsPage extends React.Component{
         <br></br>
         <div style={{backgroundColor:'#DFF3F7', paddingRight:'200px', paddingLeft: '200px',
         borderRadius: '5px', columnCount: 1, alignItems: 'center'}}>
-          <input size="50" type="text" value={this.state.search} placeholder="Search for Videos!" onChange={this.updateSearch.bind(this)} />
+          <input style={{display: 'flex', position: 'relative', left: '27px'}} size="50" type="text" value={this.state.search} placeholder="Search for Videos!" onChange={this.updateSearch.bind(this)} />
           <br></br>
           <br></br>
           <ul>
@@ -109,7 +109,7 @@ class Tutorial extends React.Component{
           url={this.props.video.url}
         />
         <h5 style={{paddingLeft: '10px'}}>{this.props.video.title}</h5>
-        <p style={{paddingLeft: '10px'}}>Posted: {this.props.video.posted} Weeks Ago</p>
+        <p style={{paddingLeft: '10px'}}>Posted: {this.props.video.posted} Week(s) Ago</p>
         <p style={{paddingLeft: '10px', paddingTop: '15px', color: 'gray'}}>{this.props.video.description}</p>
       </div>
     );
